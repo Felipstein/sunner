18514ce408688ddac4ffe9404aebdea1c5159977
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-05-08 04:20:08 -0300

    feat: add configuration information close

--- src/gateway/core/connection/handler/configuration.handler.ts
+++ src/gateway/core/connection/handler/configuration.handler.ts
@@ -1,5 +1,9 @@
+import chalk from 'chalk';
+
 import { Connection } from '..';
 import { ConnectionState } from '../../../@types/connection-state';
+import { ClientInformationPacket } from '../../../packets/client-information.packet';
+import { FinishConfigurationPacket } from '../../../packets/finish-configuration.packet';
 import { UnknownPacket } from '../../../packets/unknown-packet';
 
 import { ConnectionHandler } from '.';
@@ -12,11 +16,33 @@ export class ConfigurationConnectionHandler extends ConnectionHandler {
   override onArrivalPacket(unknownPacket: UnknownPacket) {
     switch (unknownPacket.id) {
       case 0x00: {
-        // Client Information Packet
+        const clientInformationPacket = ClientInformationPacket.fromUnknownPacket(unknownPacket);
+
+        console.log(
+          chalk.blue(
+            `User ${this.connection.encryptionStage!.username} (${clientInformationPacket.locale}) send client information. Sending Finish Configuration Packet.`,
+          ),
+        );
+
+        const finishConfigurationPacket = new FinishConfigurationPacket();
+        this.reply(finishConfigurationPacket);
+
         break;
       }
       case 0x01: {
         // Serverbound Plugin Message Packet
+        break;
+      }
+      case 0x02: {
+        // Acknowledge Finish Configuration Packet
+
+        console.log(
+          chalk.blue(
+            `User ${this.connection.encryptionStage!.username} acknowledged configuration state, starting play state.`,
+          ),
+        );
+
+        this.connection.changeState(ConnectionState.PLAY);
         break;
       }
       default: {
---
+++ src/gateway/packets/acknowledge-finish-configuration.packet.ts
@@ -0,0 +1,39 @@
+import { Packet } from '../core/packet';
+
+import { UnknownPacket } from './unknown-packet';
+
+export class AcknowledgeFinishConfigurationPacket extends Packet {
+  static readonly PACKET_ID = 0x02;
+
+  constructor() {
+    super(AcknowledgeFinishConfigurationPacket.PACKET_ID);
+  }
+
+  override get totalLength() {
+    return this.calculateLength();
+  }
+
+  override toBuffer() {
+    return this.compact();
+  }
+
+  static fromBuffer(buffer: Buffer) {
+    const { packetLength, packetId, bufferIterator } = this.abstractPacketHeader(buffer);
+
+    if (packetId !== this.PACKET_ID) {
+      throw new Error(
+        `Invalid packet ID expected for ${this.constructor.name}. Expected: ${this.PACKET_ID}, received: ${packetId}`,
+      );
+    }
+
+    const packet = new AcknowledgeFinishConfigurationPacket();
+    packet.lengthFromHeader = packetLength;
+    packet._lastOffset = bufferIterator.lastOffset;
+
+    return packet;
+  }
+
+  static fromUnknownPacket(packet: UnknownPacket) {
+    return this.fromBuffer(packet.toBuffer());
+  }
+}
