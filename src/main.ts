187a47eb58da71e71f22084ba40a6665b2a1da76
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-05-08 21:44:57 -0300

    refactor: abstract decompress function handler

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as net from 'node:net';
+import path from 'node:path';
 
 import chalk from 'chalk';
 
@@ -6,6 +7,8 @@ import { config } from './config';
 import { Connection } from './gateway/core/connection';
 import { UnknownPacket } from './gateway/packets/unknown-packet';
 import { EncryptionAuthenticationService } from './gateway/services/encryption-authentication';
+
+export const worldDir = path.resolve('tmp', 'saves', 'world');
 
 const server = net.createServer();
 export const serverKeys = EncryptionAuthenticationService.generateKeyPair();
@@ -33,44 +36,47 @@ server.on('connection', (socket) => {
     );
 
     if (unknownPacket.compressed) {
-      let offset = 0;
-      let index = 0;
-
-      while (offset < unknownPacket.totalLength) {
-        const packet = unknownPacket.slice(
-          offset,
-          index === 0 ? unknownPacket.length + 1 : undefined,
+      decompress(unknownPacket, (decompressedPacket) => {
+        console.info(
+          chalk.gray(`<<- Received packet${decipher ? chalk.blue('*') : ''}${chalk.red('*')}\t`),
+          `Packet ID: ${chalk.yellow(unknownPacket.hexId())}`,
+          '-',
+          chalk.green(connection.state),
+          '-',
+          chalk.cyan(unknownPacket.constructor.name),
+          chalk.italic(`- ( ${unknownPacket.length} bytes )`),
+          unknownPacket.compressed ? ` ${chalk.red('--')}decompressed` : '',
         );
 
-        if (!packet.compressed) {
-          console.info(
-            chalk.gray(`<<- Received packet${decipher ? chalk.blue('*') : ''}${chalk.red('*')}\t`),
-            `Packet ID: ${chalk.yellow(unknownPacket.hexId())}`,
-            '-',
-            chalk.green(connection.state),
-            '-',
-            chalk.cyan(unknownPacket.constructor.name),
-            chalk.italic(`- ( ${unknownPacket.length} bytes )`),
-            unknownPacket.compressed ? ` ${chalk.red('--')}decompressed` : '',
-          );
-
-          connection.handler.onArrivalPacket(packet);
-        } else {
-          console.warn(
-            chalk.yellow(
-              `Packet (${packet.hexId()}) is compressed. Actually the code does not support this. The package is will be ignored.`,
-            ),
-          );
-        }
-
-        offset += packet.length + 1;
-        index++;
-      }
+        connection.handler.onArrivalPacket(decompressedPacket);
+      });
     } else {
       connection.handler.onArrivalPacket(unknownPacket);
     }
   });
 });
+
+function decompress(unknownPacket: UnknownPacket, callback: (decompressed: UnknownPacket) => void) {
+  let offset = 0;
+  let index = 0;
+
+  while (offset < unknownPacket.totalLength) {
+    const packet = unknownPacket.slice(offset, index === 0 ? unknownPacket.length + 1 : undefined);
+
+    if (!packet.compressed) {
+      callback(packet);
+    } else {
+      console.warn(
+        chalk.yellow(
+          `Packet (${packet.hexId()}) is compressed. Actually the code does not support this. The package is will be ignored.`,
+        ),
+      );
+    }
+
+    offset += packet.length + 1;
+    index++;
+  }
+}
 
 server.on('error', (error) => {
   throw error;
