c86cba6b2f24dbf0aac67304d3d3c70824d222eb
Felipstein <luisfelipe-oliveira@outlook.com.br>
2024-05-11 15:14:17 -0300

    feat: add extract version from uuid and update get value method

--- src/shared/value-objects/uuid.ts
+++ src/shared/value-objects/uuid.ts
@@ -1,11 +1,19 @@
 export class UUID {
-  readonly value: string;
+  private readonly _value: string;
 
   constructor(uuid: string) {
     if (!uuid.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i)) {
       throw new Error(`Invalid UUID: ${uuid}`);
     }
 
-    this.value = uuid;
+    this._value = uuid;
+  }
+
+  get version() {
+    return parseInt(this._value.charAt(14), 16);
+  }
+
+  toString() {
+    return this._value;
   }
 }
